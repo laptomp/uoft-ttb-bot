84a37778cfcef5ffce6267f6af2daf90ec0c0aea
top <jordana884@outlook.com>
2026-07-16 15:48:40 -0400

    feat: utility functions

---
+++ src/utils/section.ts
@@ -0,0 +1,11 @@
+import { CourseSection } from "../types";
+
+export function sortSections(sections: Array<CourseSection>): Array<CourseSection> {
+	const methodOrder: Record<string, string> = { LEC: "0", TUT: "1", PRAC: "2" };
+
+	return sections.sort((a, b) => {
+		const aKey = methodOrder[a.teachMethod] + a.sectionNumber;
+		const bKey = methodOrder[b.teachMethod] + b.sectionNumber;
+		return aKey.localeCompare(bKey);
+	});
+}
